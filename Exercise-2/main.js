d44c5e47580a7126ecb1bccae812829c5a12b7c5
Deep  Patel <55467534+Deep2147@users.noreply.github.com>
2020-05-27 15:19:35 -0400

    Add files via upload

---
+++ Exercise-2/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	
+	
+
+	$("#yellowB").click(function(){
+
+		
+		$("img").attr("src", "traffic_lights/yellow_light.jpg");
+
+
+	});
+	$("#GreenB").click(function(){
+
+		
+		$("img").attr("src", "traffic_lights/green_light.jpg");
+
+
+	});
+	$("#RedB").click(function(){
+
+		
+		$("img").attr("src", "traffic_lights/red_light.jpg");
+
+
+	});
+
+});
+
